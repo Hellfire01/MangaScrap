35645f471ff182b9f2eb13c311be47b9457add54
Matthieu <matthieu.raynaud-de-fitte@epitech.eu>
2017-03-19 13:23:41 +0100

    fixed migration files and added sh for the last migration

--- migration/0.10.x_to_0.11.x.sh
+++ migration/0.10.x_to_0.11.x.sh
@@ -1,7 +1,8 @@
-sqlite3 ~/.MangaScrap/db/manga.db
-ALTER TABLE manga_trace ADD date VARCHAR(32);
+echo "ALTER TABLE manga_trace ADD date VARCHAR(32);
 ALTER TABLE manga_trace ADD nb_pages INTEGER;
 ALTER TABLE manga_todo ADD date VARCHAR(32);
 ALTER TABLE manga_list ADD date VARCHAR(32);
 ALTER TABLE manga_list ADD no_auto_updates BOOL;
 ALTER TABLE manga_list ADD ignore_todo BOOL;
+.exit
+" | sqlite3 ~/.MangaScrap/db/manga.db
